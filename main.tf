f2eacdcab4b8edf7b3c2e1539ed5f2f424ef1659
shlomi888c <100487390+shlomi888c@users.noreply.github.com>
2022-06-19 02:10:28 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,16 @@ provider "aws" {
   secret_key = "eLlpAue+6VXhLem9fpmjdNhqU3FC3XcWE4r3V7N3"
 }
 
+# Creating VPC
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name= "main-vpc"
+    Name= "main-vpc" 
  }
 }
+
+# create internet gateaway
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
@@ -19,6 +23,8 @@ resource "aws_internet_gateway" "gw" {
     Name = "main"
   }
 }
+
+# Making  Route Table
 
 resource "aws_route_table" "route-table" {
   vpc_id = aws_vpc.main.id
@@ -38,6 +44,7 @@ resource "aws_route_table" "route-table" {
   }
 }
 
+# Creating Subnet 1
 
 resource "aws_subnet" "subnet_1" {
   vpc_id     = aws_vpc.main.id
@@ -48,37 +55,33 @@ resource "aws_subnet" "subnet_1" {
    }
   }
 
-  resource "aws_route_table_association" "a" {
+# Associating the Subnet 1 with the  Route Table 1
+
+resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet_1.id
   route_table_id = aws_route_table.route-table.id
 }
+
+# Creating Security Group to Allow docker Traffic and ssh
 
 resource "aws_security_group" "allow_traffic" {
   name        = "allow traffic"
   description = "Allow web traffic"
   vpc_id      = aws_vpc.main.id
 
-
-  ingress {
-    description      = "HTTPS"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
  ingress {
     description      = "docker"
     from_port        = 5001
     to_port          = 5001
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]  
   }
  ingress {
     description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]  
   }
 
   egress {
@@ -92,28 +95,36 @@ resource "aws_security_group" "allow_traffic" {
     Name = "allow_TRAFFIC"
   }
 }
-resource "aws_network_interface" "WEB-SERVER" {
+
+# create a network interface with an ip in the subnet that was created
+
+resource "aws_network_interface" "webapp" {
   subnet_id       = aws_subnet.subnet_1.id
   private_ips     = ["10.0.1.50"]
   security_groups = [aws_security_group.allow_traffic.id]
   }
 
+# assgin an Elastic IP to the network interface
+
 resource "aws_eip" "one" {
   vpc                       = true
-  network_interface         = aws_network_interface.WEB-SERVER.id
+  network_interface         = aws_network_interface.webapp.id
   associate_with_private_ip = "10.0.1.50"
   depends_on                = [aws_internet_gateway.gw]
 }
+
+
+# create linux 2 ec2 and run
 
 resource "aws_instance" "web" {
   ami           = "ami-0cff7528ff583bf9a"
   instance_type = "t3.micro"
   availability_zone = "us-east-1a"
   key_name = "newkey"
-
+   
   network_interface {
     device_index = 0
-    network_interface_id = aws_network_interface.WEB-SERVER.id
+    network_interface_id = aws_network_interface.webapp.id
   }
 
   user_data = <<-EOF
@@ -126,12 +137,12 @@ resource "aws_instance" "web" {
               sudo curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
               sudo chmod +x /usr/local/bin/docker-compose
               sudo yum install git -y
-              sudo git clone https://github.com/shlomi888c/projectposter.git
+              sudo git clone https://github.com/shlomi888c/projectposter.git 
               cd projectposter
               sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
               sudo docker-compose build
               sudo docker-compose up
-              EOF
+              EOF 
   tags = {
    Name = "Main-ec2"
   }
